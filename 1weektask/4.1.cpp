eb8eb98131e6c7d655b30d0cd3e280dd1c434010
Сat Cheshire <catmagic000@gmail.com>
2023-06-27 18:15:18 +0300

    Create 4.1.cpp

---
+++ 1weektask/4.1.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<map>
+struct Region {
+  string std_name;
+  string parent_std_name;
+  map<Lang, string> names;
+  int64_t population;
+};
+enum class Lang {
+  DE, FR, IT
+};
+int FindMaxRepetitionCount(const vector<Region>& regions){}
+int main() {
+  cout << FindMaxRepetitionCount({
+      {
+          "Moscow",
+          "Russia",
+          {{Lang::DE, "Moskau"}, {Lang::FR, "Moscou"}, {Lang::IT, "Mosca"}},
+          89
+      }, {
+          "Russia",
+          "Eurasia",
+          {{Lang::DE, "Russland"}, {Lang::FR, "Russie"}, {Lang::IT, "Russia"}},
+          89
+      }, {
+          "Moscow",
+          "Russia",
+          {{Lang::DE, "Moskau"}, {Lang::FR, "Moscou"}, {Lang::IT, "Mosca"}},
+          89
+      }, {
+          "Moscow",
+          "Russia",
+          {{Lang::DE, "Moskau"}, {Lang::FR, "Moscou"}, {Lang::IT, "Mosca"}},
+          89
+      }, {
+          "Russia",
+          "Eurasia",
+          {{Lang::DE, "Russland"}, {Lang::FR, "Russie"}, {Lang::IT, "Russia"}},
+          89
+      },
+  }) << endl;
+
+  cout << FindMaxRepetitionCount({
+      {
+          "Moscow",
+          "Russia",
+          {{Lang::DE, "Moskau"}, {Lang::FR, "Moscou"}, {Lang::IT, "Mosca"}},
+          89
+      }, {
+          "Russia",
+          "Eurasia",
+          {{Lang::DE, "Russland"}, {Lang::FR, "Russie"}, {Lang::IT, "Russia"}},
+          89
+      }, {
+          "Moscow",
+          "Russia",
+          {{Lang::DE, "Moskau"}, {Lang::FR, "Moscou deux"}, {Lang::IT, "Mosca"}},
+          89
+      }, {
+          "Moscow",
+          "Toulouse",
+          {{Lang::DE, "Moskau"}, {Lang::FR, "Moscou"}, {Lang::IT, "Mosca"}},
+          89
+      }, {
+          "Moscow",
+          "Russia",
+          {{Lang::DE, "Moskau"}, {Lang::FR, "Moscou"}, {Lang::IT, "Mosca"}},
+          31
+      },
+  }) << endl;
+
+  return 0;
+}
