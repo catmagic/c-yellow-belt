b0a131c89ce49e496ebd95f73d1457ac9b638147
Сat Cheshire <catmagic000@gmail.com>
2023-07-03 16:58:09 +0300

    Create 2.1.cpp

---
+++ 3weektast/2.1.cpp
@@ -0,0 +1,14 @@
+#include"sum_reverse_sort.h"
+#include<algorithm>
+int Sum(int x, int y)
+{
+    return x+y;
+}
+string Reverse(string s)
+{
+    return {s.rbegin(),s.rend()};
+}
+void Sort(vector<int>& nums)
+{
+    sort(nums.begin(),nums.end());
+}
