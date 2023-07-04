b4f37a954889e97397c42b099e43132b44cef9a9
Сat Cheshire <catmagic000@gmail.com>
2023-07-04 17:58:00 +0300

    animal dog

---
+++ 5weektask/1.1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+class Animal {
+public:
+    // ваш код
+    Animal(const string &s):Name(s){}
+
+    const string Name;
+};
+
+
+class Dog :public Animal{
+public:
+    Dog(const string& s):Animal(s)
+    {}
+
+    void Bark() {
+        cout << Name << " barks: woof!" << endl;
+    }
+};
