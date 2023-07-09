fa4bbc42d765cc1d542c4379ee95c0a6f73645a2
catmagic <catmagic000@gmail.com>
2023-07-09 05:57:45 +0000

    more fast solution

--- 4weektask/4.3.cpp
+++ 4weektask/4.3.cpp
@@ -1,43 +1,34 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include<tuple>
-#include<algorithm>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <tuple>
+#include <vector>
 using namespace std;
 template <typename RandomIt>
-pair<RandomIt, RandomIt> FindStartsWith(
-    RandomIt range_begin, RandomIt range_end,
-    char prefix)
-{
-	vector<char> tmp;
-	auto it=range_begin;
-	for(;it!=range_end;++it)
-	{
-		tmp.push_back((*it)[0]);
-	}
-	int start=lower_bound(tmp.begin(),tmp.end(),prefix)-tmp.begin();
-	int end=upper_bound(tmp.begin(),tmp.end(),prefix)-tmp.begin();
-	return make_pair(range_begin+start, range_begin+end);
+pair<RandomIt, RandomIt> FindStartsWith(RandomIt range_begin,
+                                        RandomIt range_end, char prefix) {
+  return make_pair(lower_bound(range_begin, range_end, string(1,prefix)),
+                   lower_bound(range_begin, range_end, string(1,prefix + 1)));
 }
 int main() {
   const vector<string> sorted_strings = {"moscow", "murmansk", "vologda"};
-  
+
   const auto m_result =
       FindStartsWith(begin(sorted_strings), end(sorted_strings), 'm');
   for (auto it = m_result.first; it != m_result.second; ++it) {
     cout << *it << " ";
   }
   cout << endl;
-  
+
   const auto p_result =
       FindStartsWith(begin(sorted_strings), end(sorted_strings), 'p');
-  cout << (p_result.first - begin(sorted_strings)) << " " <<
-      (p_result.second - begin(sorted_strings)) << endl;
-  
+  cout << (p_result.first - begin(sorted_strings)) << " "
+       << (p_result.second - begin(sorted_strings)) << endl;
+
   const auto z_result =
       FindStartsWith(begin(sorted_strings), end(sorted_strings), 'z');
-  cout << (z_result.first - begin(sorted_strings)) << " " <<
-      (z_result.second - begin(sorted_strings)) << endl;
-  
+  cout << (z_result.first - begin(sorted_strings)) << " "
+       << (z_result.second - begin(sorted_strings)) << endl;
+
   return 0;
 }
