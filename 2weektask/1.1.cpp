05c3f120fe7292064218a63ce4b4f2c336c776ac
Replit user <>
2023-07-09 09:48:22 +0000

    fix in>>q;

--- 2weektask/1.1.cpp
+++ 2weektask/1.1.cpp
@@ -14,14 +14,14 @@ struct Query {
   string stop;
   vector<string> stops;
 };
-
 istream &operator>>(istream &is, Query &q) {
   string command;
+	
   is >> command;
   if (command == "NEW_BUS") {
     int stop_count;
     q.type = QueryType::NewBus;
-    is >> q.bus;
+    is >> q.bus>>stop_count;
     q.stops.clear();
     for (int i = 0; i < stop_count; ++i) {
       string stop;
@@ -35,6 +35,7 @@ istream &operator>>(istream &is, Query &q) {
   }
   if (command == "STOPS_FOR_BUS") {
     q.type = QueryType::StopsForBus;
+	  is>>q.bus;
   }
   if (command == "ALL_BUSES") {
     q.type = QueryType::AllBuses;
