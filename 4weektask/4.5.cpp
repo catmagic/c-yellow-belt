53bc9feec756a62ea07abf16db035e91bcbdecdd
catmagic <catmagic000@gmail.com>
2023-07-10 03:07:55 +0000

    arithmetic expression

---
+++ 4weektask/4.5.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string result;
+	cin>>result;
+	int n;
+	cin>>n;
+	for(int i=0;i<n;++i)
+	{
+		char op;
+		string number;
+		cin>>op>>number;
+		result="("+result+")"+op+number;
+		
+	}
+	cout<<result;
+	return 0;
+}
