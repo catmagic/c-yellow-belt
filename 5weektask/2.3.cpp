49655c58df8b7749ef4fb5a361c409fedcab4729
Сat Cheshire <catmagic000@gmail.com>
2023-07-25 10:45:26 +0300

    Update 2.3.cpp

--- 5weektask/2.3.cpp
+++ 5weektask/2.3.cpp
@@ -2,86 +2,122 @@
 #include <string>
 #include <vector>
 
-using namespace std;
 
-Class Man
+//--------------------------------------------------------------------------------------------------
+class Person
 {
 public:
-	Man(string name,string role):Name(name),Role(role){}
-	string role_name()
-	{
-		return Role+": "+Name;
-	}
-	void Wakl(string destination)
-	{
-		cout<<role_name()<< " walks to: " << destination << endl;
-	}
+    Person(const std::string& name, const std::string& occupation)
+        : Name(name), Occupation(occupation)
+    {
+
+    }
+
+    std::string get_name() const
+    {
+        return Name;
+    }
+
+    std::string get_occupation() const
+    {
+        return Occupation;
+    }
+    auto& log()const
+    {
+         std::cout << Occupation<<": "<< get_name()<<" ";
+         return std::cout;
+    }
+    virtual void Walk(const std::string& destination) const
+    {
+        log()<< "walks to: " << destination << std::endl;
+    }
+private:
+    const std::string Name;
+    const std::string Occupation;
+};
+//-----------------------------------------------------------------------------------------S---------
+class Student : public Person
+{
 public:
-	const string Name;
-	const string Role;
+    Student(const std::string& name, const std::string& favouriteSong)
+        : Person(name, "Student"), FavouriteSong(favouriteSong)
+    {
+
+    }
+
+    void Learn() const
+    {
+       log() << " learns" << std::endl;
+    }
+
+    void Walk(const std::string& destination) const override
+    {
+        Person::Walk(destination);
+        SingSong();
+    }
+
+    void SingSong() const
+    {
+        log() << "sings a song: " << FavouriteSong << std::endl;
+    }
+public:
+    const std::string FavouriteSong;
+};
+//--------------------------------------------------------------------------------------------------
+class Teacher : public Person
+{
+public:
+    Teacher(std::string name, std::string subject)
+        : Person(name, "Teacher"), Subject(subject)
+    {
+
+    }
+
+    void Teach() const
+    {
+        log() << "teaches: " << Subject << std::endl;
+    }
+
+
+public:
+    const std::string Subject;
+};
+//--------------------------------------------------------------------------------------------------
+class Policeman : public Person
+{
+public:
+    Policeman(const std::string& name)
+        : Person(name, "Policeman")
+    {
+
+    }
+
+    void Check(const Person& p) const
+    {
+        log() << "checks " << p.get_occupation() << ". "
+            << p.get_occupation() << "'s name is: " << p.get_name() << std::endl;
+    }
+
+
+};
+//--------------------------------------------------------------------------------------------------
+void VisitPlaces(const Person& pers, const std::vector<std::string>& places)
+{
+    for (const auto& p : places)
+    {
+        pers.Walk(p);
+    }
 }
-class Student :man{
-public:
-
-	Student(string name, string favouriteSong):Man(Name,"Student"),FavouriteSong(favouriteSong){}
-
-    void Learn() {
-        cout << role_name() << " learns" << endl;
-    }
-	void SingSong() {
-        cout << role_name()<< " sings a song: " << FavouriteSong << endl;
-    }
-    void Walk(string destination) {
-      	dynamic_cast<Man>this.Walk();
-	SingSong();
-    
-    }
-public:
-    const string FavouriteSong;
-};
-
-
-class Teacher {
-public:
-
-    Teacher(string name, string subject):Man(name,"Teacher"),Subject(subject) {}
-
-    void Teach() {
-        cout << role_name()<< " teaches: " << Subject << endl;
-    }
-
-
-public:
-    const string Subject;
-};
-
-
-class Policeman:Man {
-public:
-    Policeman(string name):Man(name,"Policeman"){}
-
-    void Check(Man* m) {
-        cout << role_name()<< " checks "<<m.Role<<". "<<m.Role<<"'s name is: " << m.Name << endl;
-    }
-};
-
-
-void VisitPlaces(Man* t, vector<string> places) {
-    for (auto p : places) {
-        t.Walk(p);
-    }
-}
-
-
-
-
-int main() {
+//--------------------------------------------------------------------------------------------------
+int main()
+{
     Teacher t("Jim", "Math");
     Student s("Ann", "We will rock you");
     Policeman p("Bob");
 
-    VisitPlaces(t, {"Moscow", "London"});
+    VisitPlaces(t, { "Moscow", "London" });
     p.Check(s);
-    VisitPlaces(s, {"Moscow", "London"});
+    VisitPlaces(s, { "Moscow", "London" });
     return 0;
 }
+//--------------------------------------------------------------------------------------------------
