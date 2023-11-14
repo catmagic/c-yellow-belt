a800eb74206716579c34b2673d94caaba3d5dce3
Сat Cheshire <catmagic000@gmail.com>
2023-11-14 10:11:32 +0300

    Update 2.5.cpp
    соответственно решение

--- 4weektask/2.5.cpp
+++ 4weektask/2.5.cpp
@@ -10,9 +10,12 @@ void MergeSort(RandomIt range_begin,RandomIt range_end)
         return;
     }
     std::vector<typename RandomIt::value_type> elements(range_begin, range_end);
-    MergeSort(elements.begin(),elements.begin()+(range_end-range_begin)/2);
-    MergeSort(elements.begin()+(range_end-range_begin)/2,elements.end());
-    std::merge(elements.begin(),elements.begin()+(range_end-range_begin)/2,elements.begin()+(range_end-range_begin)/2,elements.end(),range_begin);
+    MergeSort(elements.begin(),elements.begin()+(range_end-range_begin)/3);
+    MergeSort(elements.begin()+(range_end-range_begin)/3,elements.begin()+2*(range_end-range_begin)/3);
+    MergeSort(elements.begin()+2*(range_end-range_begin)/3,elements.end());
+    std::vector<typename RandomIt::value_type> tmp_vect;
+    std::merge(elements.begin(),elements.begin()+(range_end-range_begin)/3,elements.begin()+(range_end-range_begin)/3,elements.begin()+2*(range_end-range_begin)/3,std::back_inserter(tmp_vect));
+    std::merge(tmp_vect.begin(),tmp_vect.end(),elements.begin()+2*(range_end-range_begin)/3,elements.end(),range_begin);
 }
 int main() {
   std::vector<int> v = {6, 4, 7, 6, 4, 4, 0, 1};
